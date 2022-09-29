5a44eb07372adc2a18463676de37de628b540c1b
Leon Marteau <lmarteau01@pfm-ad.pfm.loc>
2022-09-29 15:09:11 +0200

    fasttext first try

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PRT_IA_TRIZ
+
+Fasttext URL : https://fasttext.cc/docs/en/english-vectors.html
---
+++ fasttext.py
@@ -0,0 +1,20 @@
+import io
+import scipy as sp
+import numpy as np
+def load_vectors(fname):
+    fin = io.open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+    data = {}
+    for line in fin:
+        tokens = line.rstrip().split(' ')
+        data[tokens[0]] = map(float, tokens[1:])
+    return data
+
+fasttext = load_vectors("wiki-news-300d-1M.vec")
+
+seat = list(fasttext["Uranus"])
+chair = list(fasttext["Neptune"])
+print(seat)
+print(chair)
+print()
+print(sp.spatial.distance.cosine(seat, chair))
