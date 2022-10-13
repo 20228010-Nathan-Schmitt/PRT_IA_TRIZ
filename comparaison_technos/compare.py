a6ebd0fc7120a5e77070a09e9fdc9b78e4e28c73
Leon Marteau <lmarteau01@pfm-ad.pfm.loc>
2022-10-13 17:00:22 +0200

    Intermediate results are now saved

--- comparaison_technos/compare.py
+++ comparaison_technos/compare.py
@@ -5,6 +5,9 @@ import modeles.euclide as euclide
 import cross_encoders.WMD as WMD
 from compare_tools import load_sentences, makePairsToCompare
 import tensorflow as tf
+import numpy as np
+import os.path
+
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
@@ -21,16 +24,33 @@ cross_encoders={"WMD":WMD.wmd_dist}
 
 sentences = makePairsToCompare(load_sentences())
 
+print("\n","="*50,"\n", "="*50, "\n", "="*50,"\n", sep="")
+
 for cross_encoder in cross_encoders:
+    filename = "save/cross_encoder_"+cross_encoder+".npy"
+    if os.path.isfile(filename):
+        results = np.load(filename)
+    else:
+        results = cross_encoders[cross_encoder](sentences)
+        np.save(filename, results)
     print(cross_encoder)
-    results = cross_encoders[cross_encoder](sentences)
     for i in range(len(sentences)):
         print("{:.40}  &  {:.40} =>  {}".format(sentences[i][0], sentences[i][1], results[i]))
 
+print("\n","="*50,"\n", "="*50, "\n", "="*50,"\n", sep="")
+
 for embedding in embeddings:
-    sentences_emb = embeddings[embedding](sentences)
+    filename = "save/embedding_"+embedding+".npy"
+    if os.path.isfile(filename):
+        sentences_emb = np.load(filename)
+    else:
+        sentences_emb = embeddings[embedding](sentences)
+        np.save(filename, sentences_emb)
+    print(embedding)
     for model in models:
         print(model)
         results = models[model](sentences_emb)
         for i in range(len(sentences)):
             print("{:.40}  &  {:.40} =>  {}".format(sentences[i][0], sentences[i][1], results[i]))
+        print()
+    print("="*50, "\n")
